2eb7a15733e2ce8adcdc810a37e38b1561821882
SaNeOr <sane0r@qq.com>
2022-01-04 03:08:12 +0800

    添加项目文件。

---
+++ MVVM/ViewModel/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Luluco.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Luluco.MVVM.ViewModel
+{
+    class HomeViewModel: ObservableObject
+    {
+    }
+}
---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Luluco.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Luluco.MVVM.ViewModel
+{
+    class MainViewModel: ObservableObject
+    {
+        public RelayCommands HomeViewCommand { get; set; }
+        public RelayCommands DiscoverViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoverViewModel DiscoverVM { get; set; }
+
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            DiscoverVM = new DiscoverViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommands(o => 
+            {
+                CurrentView = HomeVM;
+            });
+
+            DiscoverViewCommand = new RelayCommands(o =>
+            {
+                CurrentView = DiscoverVM;
+            });
+
+
+        }
+    }
+}
